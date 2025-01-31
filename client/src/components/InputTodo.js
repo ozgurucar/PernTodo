1f6017b3aa91985413b1220ad9313d25a4a0fb4a
Özgür Uçar <ucar.ozgur@outlook.com>
2025-01-31 18:50:08 +0300

    client:react created

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+const InputTodo = () => {
+    return <Fragment>
+        <h1 className="text-center mt-5">Pern Todo List</h1>
+    </Fragment>;
+}
+
+export default InputTodo;
